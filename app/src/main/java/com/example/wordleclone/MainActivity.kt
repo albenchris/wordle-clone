9cbba6fce37c3ba7f01c94aa34a5a1334c139096
albenchris <albenchris00@gmail.com>
2022-02-10 14:20:13 -0600

    successfully rendering gridview programmatically

--- app/src/main/java/com/example/wordleclone/MainActivity.kt
+++ app/src/main/java/com/example/wordleclone/MainActivity.kt
@@ -5,12 +5,24 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.GridView
 import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var gameBoardRow1 : GridView
+    lateinit var gridItem : TextView
+
+    var row1 = arrayListOf<String>("L", "A", "T", "E", "R")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        gameBoardRow1 = findViewById(R.id.gameBoardRow1)
+
+
+        var adapter: ArrayAdapter<String> = ArrayAdapter(this, R.layout.grid_item, R.id.gridText, row1)
+
+        gameBoardRow1.adapter = adapter
     }
 }
