5e67202379f618fe09b1e81d6b8cb6985c9fc427
albenchris <albenchris00@gmail.com>
2022-02-10 09:33:35 -0600

    initial game board, working to make gridview for changing programmatically

--- app/src/main/java/com/example/wordleclone/MainActivity.kt
+++ app/src/main/java/com/example/wordleclone/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.wordleclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.GridView
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
