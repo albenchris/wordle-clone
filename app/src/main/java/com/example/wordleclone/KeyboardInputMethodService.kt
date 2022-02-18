7e49b921ff108a4ba5927c45727d78e08c50bb58
albenchris <albenchris00@gmail.com>
2022-02-18 08:53:33 -0600

    created the KeyboardInputMethodService class

---
+++ app/src/main/java/com/example/wordleclone/KeyboardInputMethodService.kt
@@ -0,0 +1,42 @@
+package com.example.wordleclone
+
+import android.inputmethodservice.InputMethodService
+import android.inputmethodservice.KeyboardView
+
+class KeyboardInputMethodService
+    : InputMethodService(),
+        KeyboardView.OnKeyboardActionListener
+{
+    override fun onPress(primaryCode: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRelease(primaryCode: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onText(text: CharSequence?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun swipeLeft() {
+        TODO("Not yet implemented")
+    }
+
+    override fun swipeRight() {
+        TODO("Not yet implemented")
+    }
+
+    override fun swipeDown() {
+        TODO("Not yet implemented")
+    }
+
+    override fun swipeUp() {
+        TODO("Not yet implemented")
+    }
+
+}
